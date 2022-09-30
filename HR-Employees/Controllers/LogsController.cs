1a9c78537235d584ed6fe00113cf946abd60b8e8
MaRK0960 <ahmedhesham0960@outlook.com>
2022-09-30 19:41:52 +0200

    Added Logs controller and created DayLog Model.

---
+++ HR-Employees/Controllers/LogsController.cs
@@ -0,0 +1,34 @@
+﻿using HR_Employees.Models.BusinessModels;
+using HR_Employees.Models.DBModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Employees.Controllers
+{
+    public class LogsController : Controller
+    {
+        private readonly HRContext _context;
+
+        public LogsController(HRContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            var hRContext =
+                _context.Logs.
+                Where(l => l.EmployeeID == id)
+                .GroupBy(l => l.DateTime.Date)
+                .Where(g => g.Any(l => l.IsIn) && g.Any(l => !l.IsIn))
+                .Select(g => new DayLog()
+                {
+                    EmployeeName = g.First().Employee.Name,
+                    SignIn = g.Where(l => l.IsIn).OrderBy(l => l.DateTime).First().DateTime,
+                    SignOut = g.Where(l => !l.IsIn).OrderBy(l => l.DateTime).Last().DateTime,
+                });
+
+            return View(await hRContext.ToListAsync());
+        }
+    }
+}
---
+++ HR-Employees/Models/BusinessModels/DayLog.cs
@@ -0,0 +1,10 @@
+﻿namespace HR_Employees.Models.BusinessModels
+{
+    public class DayLog
+    {
+        public string EmployeeName { get; set; }
+        public DateTime SignIn { get; set; }
+        public DateTime SignOut { get; set; }
+        public TimeSpan TimeWorked => SignOut - SignIn;
+    }
+}
