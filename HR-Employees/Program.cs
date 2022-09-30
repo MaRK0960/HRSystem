15dcf6ecd6d46db53bcadde14bfa988edfb4913f
MaRK0960 <ahmedhesham0960@outlook.com>
2022-09-30 11:28:26 +0200

    Register services, and ensure database creation

--- HR-Employees/Program.cs
+++ HR-Employees/Program.cs
@@ -1,3 +1,6 @@
+using HR_Employees.Models.DBModels;
+using Microsoft.EntityFrameworkCore;
+
 namespace HR_Employees
 {
     public class Program
@@ -9,13 +12,31 @@ namespace HR_Employees
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddDbContext<HRContext>(o =>
+                o.UseSqlServer(builder.Configuration.GetConnectionString("HRContext")));
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            using (IServiceScope serviceScope = app.Services.CreateScope())
+            {
+                IServiceProvider serviceProvider = serviceScope.ServiceProvider;
+                HRContext context = serviceProvider.GetRequiredService<HRContext>();
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
             app.UseStaticFiles();
 
             app.UseRouting();
